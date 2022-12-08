6d9bb95e1cb033ad75711a69e402d17897b763f1
Sam Pak <skp3ps@virginia.edu>
2022-12-08 16:12:42 -0500

    fixed course filter

--- website/filters.py
+++ website/filters.py
@@ -3,8 +3,8 @@ from .models import Book, Course
 
 class BookFilter(django_filters.FilterSet):
     course = django_filters.ModelChoiceFilter(field_name='course', lookup_expr='exact', label='Course', distinct=True, queryset=Course.objects.all())
-    course_subj = django_filters.CharFilter(field_name='course__subject', lookup_expr='iexact', label='Course Subject', distinct=True)
-    course_cn = django_filters.CharFilter(field_name='course__class_number', lookup_expr='exact', label='Course Number', distinct=True)
+    course_subj = django_filters.CharFilter(field_name='course__subject', lookup_expr='iexact', label='Course Subject (abbr.)', distinct=True)
+    course_cn = django_filters.CharFilter(field_name='course__catalog_number', lookup_expr='exact', label='Course Number', distinct=True)
     title = django_filters.CharFilter(field_name='title', lookup_expr='icontains', label='Title', distinct=True)
     seller = django_filters.CharFilter(field_name='seller__username', lookup_expr='icontains', label='Seller', distinct=True)
     class Meta:
